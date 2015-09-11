523c729d3ce93122b52253203002625fbcebe22a
bababananaman <baurudf@icloud.com>
2015-09-11 11:43:17 +0000

    commitment

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,4 +1,17 @@
 class MessageController < ApplicationController
   def index
+    @message=Message.all
   end
+  
+  def create
+    @message=Massage.new(massage_params)
+    @message.save
+    redirect_to root_path , notice:'メッセージを保存しました'
+  end
+  
+  privete
+  def message_params
+    params.require(:message).permit(:name,:body)
+  end
+    
 end
